d7b1d169a2ef95ec7fd820e1d80cd3a8b202af91
Sherin Binu <sherinbinu@hotmail.com>
2017-05-20 10:54:41 +0530

    added delete request

--- server/server.js
+++ server/server.js
@@ -67,6 +67,27 @@ app.get('/todos/:id', (req, res) => {
   }
 });
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if(ObjectID.isValid(id)) {
+    ToDo.findByIdAndRemove(id)
+      .then((todo) => {
+        if(todo) {
+          res.send({
+            todo
+          });
+        } else {
+          res.status(404).send('No document exists with the given id');
+        }
+      })
+      .catch(err => {
+        res.status(404).send('Cant find a document with the given id');
+      });
+  } else {
+    res.status(400).send('Enter a valid ID');
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
