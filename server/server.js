5617ce17593c79aa501d7cb5f02cc165f8f14f08
Sherin Binu <sherinbinu@hotmail.com>
2017-05-20 13:56:27 +0530

    created config file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,4 @@
-let env = process.env.NODE_ENV || 'development';
-
-if(env =='development') {
-  process.env.PORT = 3000;
-  process.env.MONGOLAB_URI = 'mongodb://127.0.0.1:27017/ToDoApp';
-} else if(env == 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGOLAB_URI = 'mongodb://127.0.0.1:27017/ToDoAppTest';
-}
+require('./config/config');
 
 const {
   mongoose
