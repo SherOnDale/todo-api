14a42e9e12a3aae284a809f8544cf5485302009c
Sherin Binu <sherinbinu@hotmail.com>
2017-05-20 11:05:55 +0530

    added test for delete request

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -112,3 +112,30 @@ describe('GET /todos/:id', () => {
       .end(done);
   })
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should delete and return a todo', (done) => {
+    request(app)
+      .delete(`/todos/${todo[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todo[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 400 due to invalid id', (done) => {
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(400)
+      .end(done);
+  });
+
+  it('should return 404 due to incorrect id', (done) => {
+    let id = new ObjectID();
+    request(app)
+      .delete(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+});
